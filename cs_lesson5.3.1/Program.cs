7464285ecb2051cdf65984b885ddd44db3efc957
david <2657070472@qq.com>
2018-11-06 15:44:11 +0800

    多维数组

--- cs_lesson5.3.1/Program.cs
+++ cs_lesson5.3.1/Program.cs
@@ -10,6 +10,39 @@ namespace cs_lesson5._3._1
     {
         static void Main(string[] args)
         {
+            int[][] jaggedIntArray;
+            jaggedIntArray = new int[3][] {
+                new int[] { 1, 2, 3 }, new int[] { 1 }, new int[] { 1, 2 } };
+
+            /*
+             int[][] jaggedIntArray = { 
+             new int[] { 1, 2, 3 }, new int[] { 1 }, new int[] { 1, 2 } };
+             
+             */
+
+            int[][] divisors1To10 = { new int[] { 1 },
+            new int[] { 1, 2 },
+            new int[] { 1, 3 },
+            new int[] { 1, 2, 4 },
+            new int[] { 1, 5 },
+            new int[] { 1, 2, 3, 6 },
+            new int[] { 1, 7 },
+            new int[] { 1, 2, 4, 8 },
+            new int[] { 1, 3, 9 },
+            new int[] { 1, 2, 5, 10 } };
+            foreach (int[] divisorsOfInt in divisors1To10)
+            {
+                foreach (int divisor in divisorsOfInt)
+                {
+                    Console.WriteLine(divisor);
+                    
+                }
+                
+            }
+            Console.ReadKey();
+
+
+
         }
     }
 }
